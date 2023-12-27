9558ee9232e9865df63fff387cd764768ea3ad99
lastunicorn <coldblackstar@yahoo.com>
2023-12-27 16:23:06 +0200

    updated the copyright information.

--- sources/WindowsApi.ProcessEnv/ProcessEnv.cs
+++ sources/WindowsApi.ProcessEnv/ProcessEnv.cs
@@ -1,5 +1,5 @@
 // WindowsApi
-// Copyright (C) 2020-2022 Dust in the Wind
+// Copyright (C) 2020-2023 Dust in the Wind
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
